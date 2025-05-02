cac759b26d07f89ceb7b9d5fc8bbe7607cac59cf
Eman Radwan <emanalaa11499@gmail.com>
2025-05-02 22:23:01 +0300

    add image to user and admin schemas

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -8,6 +8,11 @@ const userSchema = new Schema(
     role: { type: String, default: "user" },
     wishlist: [{ type: Types.ObjectId, ref: "Product" }],
     isEmailActive: { type: Boolean, default: false },
+    image: {
+      type: String,
+      default:
+        "https://img.freepik.com/free-vector/illustration-user-avatar-icon_53876-5907.jpg?uid=R194767243&ga=GA1.1.1957810835.1742649565&semt=ais_hybrid&w=740",
+    },
     address: [{ type: String }],
   },
   { timestamps: true }
