3b784f90d8789a1e272f4fc6538b59cc89a2b285
Hager Amr <hageramr320@gmail.com>
2025-05-07 09:57:25 +0200

    complete categories

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,7 +2,46 @@
 import { Router } from "express";
 import categoryControllers from "../controllers/category.controller.js";
 import categoryValidation from "../validation/category.validation.js";
+import validateRequestBody from "../middlewares/schemaValidation.middleware.js";
+import authenticate from "./../middlewares/authentication.middleware.js";
+import systemRoles from "../utils/systemRoles.js";
 
 const categoryRouter = new Router();
+
+// add category
+categoryRouter.post(
+  "/add",
+  authenticate([systemRoles.admin]),
+  validateRequestBody(categoryValidation.addCategoryValidation),
+  categoryControllers.addNewCategory
+);
+
+// get all categories
+categoryRouter.get(
+  "/",
+  authenticate([systemRoles.admin, systemRoles.user]),
+  categoryControllers.getAllCategories
+);
+// get category by id
+categoryRouter.get(
+  "/:id",
+  authenticate([systemRoles.user, systemRoles.admin]),
+  categoryControllers.getCategoryById
+);
+
+// update category by id
+categoryRouter.put(
+  "/:id",
+  authenticate([systemRoles.admin]),
+  validateRequestBody(categoryValidation.updateCategoryValidation),
+  categoryControllers.updateCategory
+);
+
+// delete category by id
+categoryRouter.delete(
+  "/:id",
+  authenticate([systemRoles.admin]),
+  categoryControllers.deleteCategory
+);
 
 export default categoryRouter;
