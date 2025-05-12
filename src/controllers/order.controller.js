6da1a2a6588dcfb5f22e847f2d99c36b57955d64
Eman Radwan <emanalaa11499@gmail.com>
2025-05-12 03:15:56 +0300

    order APIs are added except posting new order API

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,11 +1,230 @@
 import Order from "../../db/models/order.model.js";
 
-const addNewOrder = (req, res) => {};
+// ^----------------------------------POST New Order--------------------------
+const addNewOrder = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "server error" });
+  }
+};
 
-const getAllOrders = (req, res) => {};
+// ^----------------------------------GET All Orders--------------------------
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate("userID", "name email image")
+      .populate({
+        path: "orderItems.productId",
+        select: "title price thumbnail material color orderCount",
+      });
 
-const getUserOrders = (req, res) => {};
+    if (orders.length === 0)
+      return res.status(404).json({ message: "no orders found" });
 
-const cancelOrder = (req, res) => {}; //additional feature
+    const allOrders = orders.map((order) => {
+      const { userID, orderItems, ...rest } = order.toObject();
 
-export default { addNewOrder, getAllOrders, getUserOrders, cancelOrder };
+      const populatedOrderItems = orderItems.map((item) => {
+        return {
+          _id: item._id,
+          product: item.productId, // Populated product data
+          quantity: item.quantity,
+        };
+      });
+
+      return {
+        ...rest,
+        user: userID,
+        orderItems: populatedOrderItems,
+      };
+    });
+
+    res.status(200).json({ message: "success", data: allOrders });
+  } catch (err) {
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+// ^----------------------------------GET All User Orders--------------------------
+const getUserOrders = async (req, res) => {
+  try {
+    const userID = req.user.id;
+
+    const orders = await Order.find({ userID })
+      .populate("userID", "name email image")
+      .populate({
+        path: "orderItems.productId",
+        select: "title price thumbnail material color orderCount",
+      });
+
+    if (orders.length === 0)
+      return res.status(404).json({ message: "no orders found" });
+
+    const allOrders = orders.map((order) => {
+      const { userID, orderItems, ...rest } = order.toObject();
+
+      const populatedOrderItems = orderItems.map((item) => {
+        return {
+          _id: item._id,
+          product: item.productId, // Populated product data
+          quantity: item.quantity,
+        };
+      });
+
+      return {
+        ...rest,
+        user: userID,
+        orderItems: populatedOrderItems,
+      };
+    });
+
+    res.status(200).json({ message: "success", data: allOrders });
+  } catch (err) {
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+// ^----------------------------------GET Order By ID--------------------------
+const getOrderByID = async (req, res) => {
+  try {
+    const orderID = req.params.id;
+
+    const order = await Order.findById(orderID)
+      .populate("userID", "name email image")
+      .populate({
+        path: "orderItems.productId",
+        select: "title price thumbnail material color orderCount",
+      });
+
+    if (!order) return res.status(404).json({ message: "order is not found" });
+
+    const { userID, orderItems, ...rest } = order.toObject();
+
+    const populatedOrderItems = orderItems.map((item) => {
+      return {
+        _id: item._id,
+        product: item.productId, // Populated product data
+        quantity: item.quantity,
+      };
+    });
+
+    const orderDetails = {
+      ...rest,
+      user: userID,
+      orderItems: populatedOrderItems,
+    };
+
+    res.status(200).json({ message: "success", data: orderDetails });
+  } catch (err) {
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+// ^----------------------------------PUT Order By ID--------------------------
+const updateOrderByID = async (req, res) => {
+  try {
+    const orderID = req.params.id;
+
+    const { shippingStatus } = req.body;
+
+    const order = await Order.findById(orderID)
+      .populate("userID", "name email image")
+      .populate({
+        path: "orderItems.productId",
+        select: "title price thumbnail material color orderCount",
+      });
+
+    if (!order) return res.status(404).json({ message: "order is not found" });
+
+    if (shippingStatus === order.shippingStatus)
+      return res.status(409).json({
+        message: `shipping status is already ${order.shippingStatus}`,
+      });
+
+    if (shippingStatus !== "shipped") {
+      order.shippingStatus = shippingStatus;
+    } else if (shippingStatus === "shipped") {
+      order.shippingStatus = shippingStatus;
+      order.orderStatus = "paid";
+    }
+
+    await order.save();
+
+    const { userID, orderItems, ...rest } = order.toObject();
+
+    const populatedOrderItems = orderItems.map((item) => {
+      return {
+        _id: item._id,
+        product: item.productId, // Populated product data
+        quantity: item.quantity,
+      };
+    });
+
+    const orderDetails = {
+      ...rest,
+      user: userID,
+      orderItems: populatedOrderItems,
+    };
+
+    res.status(200).json({ message: "success", data: orderDetails });
+  } catch (err) {
+    res.status(500).json({ message: "server error" });
+  }
+};
+// ^----------------------------------DELETE Order By ID--------------------------
+const deleteOrderByID = async (req, res) => {
+  try {
+    const orderID = req.params.id;
+
+    const order = await Order.findByIdAndDelete(orderID)
+      .populate("userID", "name email image")
+      .populate({
+        path: "orderItems.productId",
+        select: "title price thumbnail material color orderCount",
+      });
+
+    if (!order) return res.status(404).json({ message: "order is not found" });
+
+    const { userID, orderItems, ...rest } = order.toObject();
+
+    const populatedOrderItems = orderItems.map((item) => {
+      return {
+        _id: item._id,
+        product: item.productId, // Populated product data
+        quantity: item.quantity,
+      };
+    });
+
+    const orderDetails = {
+      ...rest,
+      user: userID,
+      orderItems: populatedOrderItems,
+    };
+
+    res.status(200).json({ message: "success", data: orderDetails });
+  } catch (err) {
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+// ^----------------------------------GET Current Order--------------------------
+const getCurrentOrder = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+// ^----------------------------------Cancel Order--------------------------
+const cancelOrder = async (req, res) => {}; //additional feature
+
+export default {
+  getAllOrders,
+  getUserOrders,
+  getOrderByID,
+  updateOrderByID,
+  deleteOrderByID,
+  addNewOrder,
+  getCurrentOrder,
+  cancelOrder,
+};
