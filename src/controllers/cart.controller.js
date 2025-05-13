ec124e0d05f7943f1e162f338c27f2ca14dbb642
Nour Ashraf <nourelhudaashraf20@gmail.com>
2025-05-13 18:48:07 +0300

    edit cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -3,10 +3,7 @@ import Cart from "../../db/models/cart.model.js";
 const addProductToCart = async (req, res, userID) => {
   try {
     //authorize user
-    if (!userID)
-      return res
-        .status(401)
-        .json({ message: "you are not authorized to get this content" });
+    if (!userID) return res.status(401).json({ message: "you are not authorized to get this content" });
     //get cart of user
     let cart = await Cart.findOne({ userID });
     let { productId } = req.body;
@@ -24,9 +21,7 @@ const addProductToCart = async (req, res, userID) => {
     }
 
     //the product exists in cart items or not
-    const existingItem = cart.cartItems.find(
-      (item) => item.productId.toString() === productId
-    );
+    const existingItem = cart.cartItems.find((item) => item.productId.toString() === productId);
     if (existingItem) {
       existingItem.quantity += 1;
     } else {
@@ -45,10 +40,7 @@ const addProductToCart = async (req, res, userID) => {
 
 const getUserCart = async (req, res, userID) => {
   try {
-    if (!userID)
-      return res
-        .status(401)
-        .json({ message: "you are not authorized to get this content" });
+    if (!userID) return res.status(401).json({ message: "you are not authorized to get this content" });
     //get cart with product data
     let cart = await Cart.findOne({ userID }).populate({
       path: "cartItems.productId",
@@ -78,10 +70,7 @@ const getUserCart = async (req, res, userID) => {
 
 const updateCartItem = async (req, res, userID) => {
   try {
-    if (!userID)
-      return res
-        .status(401)
-        .json({ message: "you are not authorized to get this content" });
+    if (!userID) return res.status(401).json({ message: "you are not authorized to get this content" });
     //get cart of user
     let cart = await Cart.findOne({ userID }).populate({
       path: "cartItems.productId",
@@ -96,12 +85,7 @@ const updateCartItem = async (req, res, userID) => {
       return res.status(400).json({ message: "quantity must be at least 1" });
     }
 
-
     const existingItem = cart.cartItems.find((item) => item.productId._id.toString() === productId);
-
-    const existingItem = cart.cartItems.find(
-      (item) => item.productId.toString() === productId
-    );
 
     if (!existingItem) {
       return res.status(404).json({ message: "product not found in cart" });
@@ -119,10 +103,7 @@ const updateCartItem = async (req, res, userID) => {
 
 const deleteCartItem = async (req, res, userID) => {
   try {
-    if (!userID)
-      return res
-        .status(401)
-        .json({ message: "you are not authorized to get this content" });
+    if (!userID) return res.status(401).json({ message: "you are not authorized to get this content" });
 
     let cart = await Cart.findOne({ userID }).populate({
       path: "cartItems.productId",
@@ -132,12 +113,7 @@ const deleteCartItem = async (req, res, userID) => {
 
     let { productId } = req.params;
 
-
     const existingItem = cart.cartItems.find((item) => item.productId._id.toString() === productId);
-
-    const existingItem = cart.cartItems.find(
-      (item) => item.productId.toString() === productId
-    );
 
     if (!existingItem) {
       return res.status(404).json({ message: "product not found in cart" });
@@ -145,10 +121,6 @@ const deleteCartItem = async (req, res, userID) => {
     //filter to remove item from cart
 
     const filteredCart = cart.cartItems.filter((item) => item.productId._id.toString() !== productId);
-
-    const filteredCart = cart.cartItems.filter(
-      (item) => item.productId.toString() !== productId
-    );
 
     if (filteredCart.length !== cart.cartItems.length) {
       cart.cartItems = filteredCart;
@@ -165,10 +137,7 @@ const deleteCartItem = async (req, res, userID) => {
 
 const clearCart = async (req, res, userID) => {
   try {
-    if (!userID)
-      return res
-        .status(401)
-        .json({ message: "you are not authorized to get this content" });
+    if (!userID) return res.status(401).json({ message: "you are not authorized to get this content" });
 
     let cart = await Cart.findOne({ userID });
     if (!cart) return res.status(404).json({ message: "no cart for user" });
