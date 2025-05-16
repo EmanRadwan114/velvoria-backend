5106c8c27920eae0f2449343dc2c4fd18e8d5bad
Nouran-Alaa <nouranalaa81@gmail.com>
2025-05-16 07:48:21 +0300

    Update Products Pagination

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,11 +4,28 @@ import mongoose from "mongoose";
 
 const getAllProducts = async (req, res) => {
   try {
-    let products = await Product.find().sort({ createdAt: -1 });
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 6;
+    const skip = (page - 1) * limit;
+
+    const total = await Product.countDocuments();
+    const totalPages = Math.ceil(total / limit);
+
+    let products = await Product.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
     if (products.length === 0) {
       res.status(404).json({ message: "no products found" });
     }
-    res.status(200).json({ message: "success", data: products });
+    res
+      .status(200)
+      .json({
+        message: "success",
+        data: products,
+        currentPage: page,
+        totalPages,
+      });
   } catch (err) {
     res.status(500).json({ message: "server error" });
   }
